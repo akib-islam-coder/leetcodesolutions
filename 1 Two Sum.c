91d566505232848f9fe0531c775a9e6551df76f2
akib-islam-coder <akibislam2021@gmail.com>
2021-05-19 11:41:38 +0530

    Commit #3 - Modification to solution of problem #1
    - In earlier approach, I was creating the hashtable, filling all the enteries and then start the search
    - In modified approach, we can insert elements in hashtable on-the-fly during searching elements. If corresponding element (target - nums[i]) is not present, then insert the element
      in the hashtable.
    
      Runtime: 12 ms, faster than 32.15% of C online submissions for Two Sum.
      Memory Usage: 6.9 MB, less than 6.11% of C online submissions for Two Sum.
    
      signed-off-by : Akib Islam <akibislam2021@gmail.com>

--- 1 Two Sum.c
+++ 1 Two Sum.c
@@ -24,7 +24,7 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize)
     * If yes, then the element found at hash table and nums[i] adds up to the target. If no, continue.
     */
     
-    int i = 0, count=0;
+    int i = 0;
     bool match_found = false;
     
     *returnSize = 2;
@@ -33,52 +33,16 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize)
     
     memset(hashtable, NULL, 100 * sizeof(Node *));
     
-    //Insert all the items in hashtable
-    for(i=0; i<numsSize; i++)
-    {
-        int index = hash_index(nums[i]);
-        if(index < 0)
-        {
-            index = (-1 * index);
-        }
-        
-        Node * new_node = (Node *)malloc(sizeof(Node));
-        new_node->data  = nums[i];
-        new_node->index = i;
-        new_node->next  = NULL;
-    
-        
-        if(hashtable[index] == NULL)
-        {
-            hashtable[index] = new_node;    
-        }
-        else
-        {
-            Node * trav_ptr = hashtable[index];
-            
-            while(trav_ptr->next != NULL)   //Not inserting in sorting order
-            {
-                trav_ptr = trav_ptr->next;
-            }
-            
-            trav_ptr->next = new_node;
-        }
-    }
-    
     i=0;
     int search_element;
     
     for(i=0; i<numsSize && (match_found==false); i++)
     {
-        count = 0;
-        search_element = 0;
-        search_element = target - nums[i];
-        
-        ret_arr[0] = i;
+        search_element = target -  nums[i];  
+        ret_arr[1] = i;
         
         //Search the search_element in the hash table
-        int index;
-        
+        int index;        
         if(search_element < 0)
         {
             index = hash_index(-1 * search_element);
@@ -90,41 +54,51 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize)
 
         Node * trav_ptr = hashtable[index];
         
-        if(search_element == nums[i])   //Two elements of same data should exists if successful solution
+        while(trav_ptr != NULL)
         {
-            count = 0; 
-
-            while(trav_ptr != NULL)
+            if(trav_ptr->data == search_element)    //If corresponding element is found, break
             {
-                if(trav_ptr->data == search_element)
-                {
-                    count++;
-                }
-                
-                if(count == 2)
-                {
-                    ret_arr[1] =  trav_ptr->index;
-                    match_found = true;
-                    break;
-                }
-                
+                match_found = true;
+                ret_arr[0] =  trav_ptr->index;
+                break;
+            }
+            else                                    //Else iterate the linkedlist
+            {
                 trav_ptr = trav_ptr->next;
             }
         }
-        else
+        
+        if(!match_found)    //If match is not found, insert the nums[i] in the hashtable
         {
-            while(trav_ptr != NULL)
+            index = hash_index(nums[i]);
+            if(index < 0)
             {
-                if(trav_ptr->data == search_element)
+                index = (-1 * index);
+            }
+        
+            Node * new_node = (Node *)malloc(sizeof(Node));
+            new_node->data  = nums[i];
+            new_node->index = i;
+            new_node->next  = NULL;
+    
+        
+            if(hashtable[index] == NULL)
+            {
+                hashtable[index] = new_node;    
+            }
+            else
+            {
+                Node * trav_ptr = hashtable[index];
+            
+                while(trav_ptr->next != NULL)   //Not inserting in sorting order
                 {
-                    ret_arr[1] =  trav_ptr->index;
-                    match_found = true;
-                    break;
+                    trav_ptr = trav_ptr->next;
                 }
-                
-                trav_ptr = trav_ptr->next;
+            
+                trav_ptr->next = new_node;
             }
         }
+        
     }
     
     return ret_arr;
